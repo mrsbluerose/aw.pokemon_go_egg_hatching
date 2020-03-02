3b538343387337343480bc05a3e6d7fff461631b
mrsbluerose <mrsbluerose@gmail.com>
2020-03-02 15:22:24 -0700

    updated inventory with setters/getters

--- src/pokemongoeggs/Inventory.java
+++ src/pokemongoeggs/Inventory.java
@@ -25,6 +25,73 @@ public class Inventory {
 
 	private int incubatorRegularQty;
 	private int incubatorSuperQty;
+	
+	public Egg getEgg1() {
+		return egg1;
+	}
+	public void setEgg1(Egg egg1) {
+		this.egg1 = egg1;
+	}
+	public Egg getEgg2() {
+		return egg2;
+	}
+	public void setEgg2(Egg egg2) {
+		this.egg2 = egg2;
+	}
+	public Egg getEgg3() {
+		return egg3;
+	}
+	public void setEgg3(Egg egg3) {
+		this.egg3 = egg3;
+	}
+	public Egg getEgg4() {
+		return egg4;
+	}
+	public void setEgg4(Egg egg4) {
+		this.egg4 = egg4;
+	}
+	public Egg getEgg5() {
+		return egg5;
+	}
+	public void setEgg5(Egg egg5) {
+		this.egg5 = egg5;
+	}
+	public Egg getEgg6() {
+		return egg6;
+	}
+	public void setEgg6(Egg egg6) {
+		this.egg6 = egg6;
+	}
+	public Egg getEgg7() {
+		return egg7;
+	}
+	public void setEgg7(Egg egg7) {
+		this.egg7 = egg7;
+	}
+	public Egg getEgg8() {
+		return egg8;
+	}
+	public void setEgg8(Egg egg8) {
+		this.egg8 = egg8;
+	}
+	public Egg getEgg9() {
+		return egg9;
+	}
+	public void setEgg9(Egg egg9) {
+		this.egg9 = egg9;
+	}
+	public int getIncubatorRegularQty() {
+		return incubatorRegularQty;
+	}
+	public void setIncubatorRegularQty(int incubatorRegularQty) {
+		this.incubatorRegularQty = incubatorRegularQty;
+	}
+	public int getIncubatorSuperQty() {
+		return incubatorSuperQty;
+	}
+	public void setIncubatorSuperQty(int incubatorSuperQty) {
+		this.incubatorSuperQty = incubatorSuperQty;
+	}
 
 	//
 //	private int egg10KMQty;
