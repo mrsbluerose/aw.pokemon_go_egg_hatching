9c5ede4456e2ad53df866213f80fca098326b545
mrsbluerose <mrsbluerose@gmail.com>
2020-04-20 13:08:40 -0600

    Creates menu and options

--- src/pokemongoeggs/PokemonGoEggsGUI.java
+++ src/pokemongoeggs/PokemonGoEggsGUI.java
@@ -3,6 +3,9 @@ package pokemongoeggs;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JRadioButton;
 
 public class PokemonGoEggsGUI {
 
@@ -38,6 +41,22 @@ public class PokemonGoEggsGUI {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Menu");
+		lblNewLabel.setBounds(12, 12, 265, 15);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JRadioButton rdbtnDisplayEggHatching = new JRadioButton("Display egg hatching table");
+		rdbtnDisplayEggHatching.setBounds(12, 35, 307, 23);
+		frame.getContentPane().add(rdbtnDisplayEggHatching);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Calculate when an egg will hatch");
+		rdbtnNewRadioButton.setBounds(12, 62, 307, 23);
+		frame.getContentPane().add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Create a plan for 9 eggs and incubators");
+		rdbtnNewRadioButton_1.setBounds(12, 89, 307, 23);
+		frame.getContentPane().add(rdbtnNewRadioButton_1);
 	}
-
 }
