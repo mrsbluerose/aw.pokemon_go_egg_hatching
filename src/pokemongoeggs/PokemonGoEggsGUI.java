f7123b42751fddc52f5e8770e7574216d7f1deec
mrsbluerose <mrsbluerose@gmail.com>
2020-06-08 15:18:52 -0600

    Adds Incubator choices

--- src/pokemongoeggs/PokemonGoEggsGUI.java
+++ src/pokemongoeggs/PokemonGoEggsGUI.java
@@ -12,12 +12,20 @@ import javax.swing.ButtonGroup;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JLayeredPane;
 
 public class PokemonGoEggsGUI {
 
 	private JFrame frame;
 	private final ButtonGroup buttonGroupMenu = new ButtonGroup();
 	private final ButtonGroup buttonGroupEvent = new ButtonGroup();
+	private final ButtonGroup buttonGroupIncubator = new ButtonGroup();
+	
 
 	/**
 	 * Launch the application.
@@ -47,9 +55,10 @@ public class PokemonGoEggsGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 438);
+		frame.setBounds(100, 100, 450, 536);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
 		
 		//main menu
 		JLabel lblMenu = new JLabel("Menu");
@@ -61,39 +70,68 @@ public class PokemonGoEggsGUI {
 		rdbtnDisplayTable.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				mnEventMenu.setVisible(true);
+				//mnEventMenu.setVisible(true);
 			}
 		});
-		rdbtnDisplayTable.setBounds(12, 35, 307, 23);
+		rdbtnDisplayTable.setBounds(46, 35, 307, 23);
 		frame.getContentPane().add(rdbtnDisplayTable);
 		
 		JRadioButton rdbtnCalculateEgg = new JRadioButton("Calculate when an egg will hatch");
 		buttonGroupMenu.add(rdbtnCalculateEgg);
-		rdbtnCalculateEgg.setBounds(12, 62, 307, 23);
+		rdbtnCalculateEgg.setBounds(46, 62, 307, 23);
 		frame.getContentPane().add(rdbtnCalculateEgg);
 		
 		JRadioButton rdbtnCreatePlan = new JRadioButton("Create a plan for 9 eggs and incubators");
 		buttonGroupMenu.add(rdbtnCreatePlan);
-		rdbtnCreatePlan.setBounds(12, 89, 307, 23);
+		rdbtnCreatePlan.setBounds(46, 89, 307, 23);
 		frame.getContentPane().add(rdbtnCreatePlan);
 		
+		//Options for all choices
+		
+		//Get event information
 		JLabel lblGetEvent = new JLabel("What is the event walking distance?");
 		lblGetEvent.setBounds(12, 133, 290, 15);
 		frame.getContentPane().add(lblGetEvent);
 		
 		JRadioButton rdbtnOneQuarter = new JRadioButton("1/4");
 		buttonGroupEvent.add(rdbtnOneQuarter);
-		rdbtnOneQuarter.setBounds(316, 129, 61, 23);
+		rdbtnOneQuarter.setBounds(46, 156, 61, 23);
 		frame.getContentPane().add(rdbtnOneQuarter);
 		
 		JRadioButton rdbtnOneHalf = new JRadioButton("1/2");
 		buttonGroupEvent.add(rdbtnOneHalf);
-		rdbtnOneHalf.setBounds(381, 129, 61, 23);
+		rdbtnOneHalf.setBounds(159, 156, 61, 23);
 		frame.getContentPane().add(rdbtnOneHalf);
 		
+		JRadioButton rdbtnNoEvent = new JRadioButton("no event");
+		rdbtnNoEvent.setBounds(262, 156, 149, 23);
+		frame.getContentPane().add(rdbtnNoEvent);
+		
+
+		//Get incubator information
 		JLabel lblIncubatorType = new JLabel("Use regular incubators, super or both?");
-		lblIncubatorType.setBounds(12, 161, 286, 15);
+		lblIncubatorType.setBounds(12, 198, 286, 15);
 		frame.getContentPane().add(lblIncubatorType);
+		
+		JRadioButton rdbtnRegular = new JRadioButton("regular");
+		buttonGroupIncubator.add(rdbtnRegular);
+		rdbtnRegular.setBounds(46, 221, 101, 23);
+		frame.getContentPane().add(rdbtnRegular);
+		
+		JRadioButton rdbtnSuper = new JRadioButton("super");
+		buttonGroupIncubator.add(rdbtnSuper);
+		rdbtnSuper.setBounds(159, 221, 101, 23);
+		frame.getContentPane().add(rdbtnSuper);
+		
+		JRadioButton rdbtnBoth = new JRadioButton("both");
+		buttonGroupIncubator.add(rdbtnBoth);
+		rdbtnBoth.setBounds(262, 221, 149, 23);
+		frame.getContentPane().add(rdbtnBoth);
+	
+
+		
+
+		
 		
 
 	}
