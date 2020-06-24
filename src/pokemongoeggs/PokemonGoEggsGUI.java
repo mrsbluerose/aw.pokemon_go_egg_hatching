d547eb4b57438e461ea25e6a34269aed927cd964
mrsbluerose <mrsbluerose@gmail.com>
2020-06-23 18:13:44 -0600

    creates panels for options and sets visibility to false.

--- src/pokemongoeggs/PokemonGoEggsGUI.java
+++ src/pokemongoeggs/PokemonGoEggsGUI.java
@@ -100,63 +100,64 @@ public class PokemonGoEggsGUI {
 		textAreaInstructions.setBounds(12, 132, 426, 70);
 		frame.getContentPane().add(textAreaInstructions);
 		
-		JPanel eggOptionsPanel = new JPanel();
-		eggOptionsPanel.setBounds(15, 241, 423, 91);
-		frame.getContentPane().add(eggOptionsPanel);
-		eggOptionsPanel.setLayout(null);
+		JPanel panelEggOptions = new JPanel();
+		panelEggOptions.setBounds(15, 241, 423, 91);
+		frame.getContentPane().add(panelEggOptions);
+		panelEggOptions.setLayout(null);
+		panelEggOptions.setVisible(false);
 		
 		JLabel label = new JLabel(".");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setVerticalAlignment(SwingConstants.BOTTOM);
 		label.setBounds(189, 60, 18, 19);
-		eggOptionsPanel.add(label);
+		panelEggOptions.add(label);
 		
 		JButton btnNextEgg = new JButton("Enter another egg");
 		btnNextEgg.setBounds(259, 28, 164, 25);
-		eggOptionsPanel.add(btnNextEgg);
+		panelEggOptions.add(btnNextEgg);
 		
 		JButton btnDisplayResults = new JButton("Display Results");
 		btnDisplayResults.setBounds(259, 66, 164, 25);
-		eggOptionsPanel.add(btnDisplayResults);
+		panelEggOptions.add(btnDisplayResults);
 		
 		JLabel lblEggType = new JLabel("Egg Type");
 		lblEggType.setHorizontalAlignment(SwingConstants.LEFT);
 		lblEggType.setBounds(12, 0, 128, 26);
-		eggOptionsPanel.add(lblEggType);
+		panelEggOptions.add(lblEggType);
 		
 		JLabel lblIncubatorType = new JLabel("Incubator Type");
 		lblIncubatorType.setHorizontalAlignment(SwingConstants.LEFT);
 		lblIncubatorType.setBounds(12, 27, 128, 26);
-		eggOptionsPanel.add(lblIncubatorType);
+		panelEggOptions.add(lblIncubatorType);
 		
 		JLabel lblDistanceWalked = new JLabel("Distance Walked");
 		lblDistanceWalked.setHorizontalAlignment(SwingConstants.LEFT);
 		lblDistanceWalked.setBackground(Color.WHITE);
 		lblDistanceWalked.setBounds(12, 55, 128, 26);
-		eggOptionsPanel.add(lblDistanceWalked);
+		panelEggOptions.add(lblDistanceWalked);
 		
 		String[] eggTypes = { " ", "2 KM", "5 KM", "7 KM", "10 KM"};
 		JComboBox comboBoxEggType = new JComboBox(eggTypes);
 		comboBoxEggType.setBackground(Color.WHITE);
 		comboBoxEggType.setBounds(149, 1, 98, 24);
-		eggOptionsPanel.add(comboBoxEggType);
+		panelEggOptions.add(comboBoxEggType);
 		
 		String[] incubatorTypes = { "regular", "super"};
 		JComboBox comboBoxIncubatorType = new JComboBox(incubatorTypes);
 		comboBoxIncubatorType.setBackground(Color.WHITE);
 		comboBoxIncubatorType.setBounds(149, 28, 98, 24);
-		eggOptionsPanel.add(comboBoxIncubatorType);
+		panelEggOptions.add(comboBoxIncubatorType);
 		
 		String[] distanceNumbers = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 		JComboBox comboBoxDistanceWalkedNumber = new JComboBox(distanceNumbers);
 		comboBoxDistanceWalkedNumber.setBackground(Color.WHITE);
 		comboBoxDistanceWalkedNumber.setBounds(149, 55, 40, 24);
-		eggOptionsPanel.add(comboBoxDistanceWalkedNumber);
+		panelEggOptions.add(comboBoxDistanceWalkedNumber);
 		
 		JComboBox comboBoxDistanceWalkedDecimal = new JComboBox(distanceNumbers);
 		comboBoxDistanceWalkedDecimal.setBackground(Color.WHITE);
 		comboBoxDistanceWalkedDecimal.setBounds(207, 55, 40, 24);
-		eggOptionsPanel.add(comboBoxDistanceWalkedDecimal);
+		panelEggOptions.add(comboBoxDistanceWalkedDecimal);
 		
 		JButton btnReset = new JButton("Reset");
 		btnReset.setBounds(199, 524, 117, 25);
@@ -171,14 +172,20 @@ public class PokemonGoEggsGUI {
 		frame.getContentPane().add(textAreaResults);
 		
 		String[] eventDistanceArray = {"no event", "1/4", "1/2"};
+		
+		JPanel panelEvent = new JPanel();
+		panelEvent.setBounds(12, 205, 426, 24);
+		frame.getContentPane().add(panelEvent);
+		panelEvent.setLayout(null);
 		JComboBox comboBoxEventWalkingDistance = new JComboBox(eventDistanceArray);
+		comboBoxEventWalkingDistance.setBounds(336, 0, 90, 24);
+		panelEvent.add(comboBoxEventWalkingDistance);
 		comboBoxEventWalkingDistance.setBackground(Color.WHITE);
-		comboBoxEventWalkingDistance.setBounds(348, 205, 90, 24);
-		frame.getContentPane().add(comboBoxEventWalkingDistance);
+		panelEvent.setVisible(false);
 		
 		JLabel lblEventWalkingDistance = new JLabel("What is the event walking distance multiplier?");
-		lblEventWalkingDistance.setBounds(12, 205, 341, 24);
-		frame.getContentPane().add(lblEventWalkingDistance);
+		lblEventWalkingDistance.setBounds(0, 0, 341, 24);
+		panelEvent.add(lblEventWalkingDistance);
 	
 
 		
