cc55fd6f2be79abb4ea71dca75453827a0fd81e1
mrsbluerose <mrsbluerose@gmail.com>
2020-07-10 12:30:45 -0600

    Adds method to calculate selected values, removes unused imports

--- src/pokemongoeggs/PokemonGoEggsGUI.java
+++ src/pokemongoeggs/PokemonGoEggsGUI.java
@@ -18,9 +18,6 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import java.awt.Color;
 import javax.swing.JButton;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import javax.swing.JTable;
 import javax.swing.JScrollPane;
 import java.awt.event.ItemListener;
 import java.awt.event.ItemEvent;
@@ -81,35 +78,6 @@ class PokemonGoEggsGUI {
 		textAreaResults.setBounds(12, 344, 426, 176);
 		frame.getContentPane().add(textAreaResults);
 		
-		//Create Event Panel
-//		TODO: get event types from class instead of array
-		String[] eventDistanceArray = {"no event", "1/4", "1/2"};
-
-		JPanel panelEvent = new JPanel();
-		panelEvent.setBounds(12, 205, 426, 24);
-		frame.getContentPane().add(panelEvent);
-		panelEvent.setLayout(null);
-		
-		JComboBox comboBoxEventWalkingDistance = new JComboBox(eventDistanceArray);
-		comboBoxEventWalkingDistance.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent e) {
-				if (e.getSource() == comboBoxEventWalkingDistance) {
-					//eventMultiplier = setText(comboBoxEventWalkingDistance.getSelectedItem().toString());
-				}
-			}
-		});
-
-//		TODO: determine whether to use objects and set properties or arrays to reference. Such as: egg, set distance or egg and pick distance
-
-		comboBoxEventWalkingDistance.setBounds(336, 0, 90, 24);
-		panelEvent.add(comboBoxEventWalkingDistance);
-		comboBoxEventWalkingDistance.setBackground(Color.WHITE);
-		panelEvent.setVisible(false);
-		
-		JLabel lblEventWalkingDistance = new JLabel("What is the event walking distance multiplier?");
-		lblEventWalkingDistance.setBounds(0, 0, 341, 24);
-		panelEvent.add(lblEventWalkingDistance);
-		
 		//Create Egg information panel
 		JPanel panelEggOptions = new JPanel();
 		panelEggOptions.setBounds(15, 241, 423, 81);
@@ -169,6 +137,97 @@ class PokemonGoEggsGUI {
 		comboBoxDistanceWalkedDecimal.setBounds(207, 55, 40, 24);
 		panelEggOptions.add(comboBoxDistanceWalkedDecimal);
 		
+		//Create Event Panel
+//		TODO: get event types from class instead of array
+		String[] eventDistanceArray = {"no event", "1/4", "1/2"};
+
+		JPanel panelEvent = new JPanel();
+		panelEvent.setBounds(12, 205, 426, 24);
+		frame.getContentPane().add(panelEvent);
+		panelEvent.setLayout(null);
+		
+		JComboBox comboBoxEventWalkingDistance = new JComboBox(eventDistanceArray);
+		comboBoxEventWalkingDistance.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getSource() == comboBoxEventWalkingDistance) {
+					eventMultiplier = getValue(comboBoxEventWalkingDistance.getSelectedItem().toString());
+					System.out.println("The event is: " + eventMultiplier); 
+				}
+			}
+		});
+//		eventMultiplier = getValue(comboBoxEventWalkingDistance.getSelectedItem().toString());
+//		System.out.println("The event is: " + eventMultiplier);
+		
+//		TODO: determine whether to use objects and set properties or arrays to reference. Such as: egg, set distance or egg and pick distance
+
+		comboBoxEventWalkingDistance.setBounds(336, 0, 90, 24);
+		panelEvent.add(comboBoxEventWalkingDistance);
+		comboBoxEventWalkingDistance.setBackground(Color.WHITE);
+		panelEvent.setVisible(false);
+		
+		JLabel lblEventWalkingDistance = new JLabel("What is the event walking distance multiplier?");
+		lblEventWalkingDistance.setBounds(0, 0, 341, 24);
+		panelEvent.add(lblEventWalkingDistance);
+		
+//		//Create Egg information panel
+//		JPanel panelEggOptions = new JPanel();
+//		panelEggOptions.setBounds(15, 241, 423, 81);
+//		frame.getContentPane().add(panelEggOptions);
+//		panelEggOptions.setLayout(null);
+//		panelEggOptions.setVisible(false);
+//		
+//		JLabel label = new JLabel(".");
+//		label.setHorizontalAlignment(SwingConstants.CENTER);
+//		label.setVerticalAlignment(SwingConstants.BOTTOM);
+//		label.setBounds(189, 60, 18, 19);
+//		panelEggOptions.add(label);
+//		
+//		JButton btnNextEgg = new JButton("Enter another egg");
+//		btnNextEgg.setBounds(259, 54, 164, 25);
+//		panelEggOptions.add(btnNextEgg);
+//		
+//		JLabel lblEggType = new JLabel("Egg Type");
+//		lblEggType.setHorizontalAlignment(SwingConstants.LEFT);
+//		lblEggType.setBounds(12, 0, 128, 26);
+//		panelEggOptions.add(lblEggType);
+//		
+//		JLabel lblIncubatorType = new JLabel("Incubator Type");
+//		lblIncubatorType.setHorizontalAlignment(SwingConstants.LEFT);
+//		lblIncubatorType.setBounds(12, 27, 128, 26);
+//		panelEggOptions.add(lblIncubatorType);
+//		
+//		JLabel lblDistanceWalked = new JLabel("Distance Walked");
+//		lblDistanceWalked.setHorizontalAlignment(SwingConstants.LEFT);
+//		lblDistanceWalked.setBackground(Color.WHITE);
+//		lblDistanceWalked.setBounds(12, 55, 128, 26);
+//		panelEggOptions.add(lblDistanceWalked);
+//		
+////		TODO: get egg types from class instead of array		
+//		String[] eggTypes = { " ", "2 KM", "5 KM", "7 KM", "10 KM"};
+//		JComboBox comboBoxEggType = new JComboBox(eggTypes);
+//		comboBoxEggType.setBackground(Color.WHITE);
+//		comboBoxEggType.setBounds(149, 1, 98, 24);
+//		panelEggOptions.add(comboBoxEggType);
+//		
+////		TODO: get incubator types from class instead of array		
+//		String[] incubatorTypes = { "regular", "super"};
+//		JComboBox comboBoxIncubatorType = new JComboBox(incubatorTypes);
+//		comboBoxIncubatorType.setBackground(Color.WHITE);
+//		comboBoxIncubatorType.setBounds(149, 28, 98, 24);
+//		panelEggOptions.add(comboBoxIncubatorType);
+//		
+////		TODO: get distance from class instead of array	
+//		String[] distanceNumbers = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+//		JComboBox comboBoxDistanceWalkedNumber = new JComboBox(distanceNumbers);
+//		comboBoxDistanceWalkedNumber.setBackground(Color.WHITE);
+//		comboBoxDistanceWalkedNumber.setBounds(149, 55, 40, 24);
+//		panelEggOptions.add(comboBoxDistanceWalkedNumber);
+//		
+//		JComboBox comboBoxDistanceWalkedDecimal = new JComboBox(distanceNumbers);
+//		comboBoxDistanceWalkedDecimal.setBackground(Color.WHITE);
+//		comboBoxDistanceWalkedDecimal.setBounds(207, 55, 40, 24);
+//		panelEggOptions.add(comboBoxDistanceWalkedDecimal);
+		
 //		//Creates text areas
 //		JTextArea textAreaInstructions = new JTextArea();
 //		textAreaInstructions.setBounds(12, 132, 426, 70);
@@ -190,6 +249,7 @@ class PokemonGoEggsGUI {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				panelEvent.setVisible(true);
+				panelEggOptions.setVisible(false);
 				textAreaInstructions.setText(displayInstructions(1));
 						
 			}
@@ -252,6 +312,11 @@ class PokemonGoEggsGUI {
 		frame.getContentPane().add(btnExit);
 		
 		JButton btnDisplayResults = new JButton("Display Results");
+		btnDisplayResults.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		btnDisplayResults.setBounds(44, 524, 164, 25);
 		frame.getContentPane().add(btnDisplayResults);
 		
@@ -276,10 +341,42 @@ class PokemonGoEggsGUI {
 								+ "If you want to enter more than one egg, click \"enter another egg\"\n"
 								+ "When you are finished, click \"display results\"";
 					break;
-			default: result = "please choose again";
+			case 4: result = "Please select an event distance multiplier";
+					break;
+			default: result = "error";
 		}
 		
 		return result;
+	}
+	
+	private double getValue(String choice) {
+		double value = 1; //no event, regular incubator
+		
+		if (choice.contentEquals("1/4")){
+			value = 0.25;
+		}
+		else if (choice.contentEquals("1/2")){
+			value = 0.5;
+		}
+		else if (choice.contentEquals("2 KM")) {
+			value = 2.0;
+		}
+		else if (choice.contentEquals("5 KM")) {
+			value = 5.0;
+		}
+		else if (choice.contentEquals("7 KM")) {
+			value = 7.0;
+		}
+		else if (choice.contentEquals("10 KM")) {
+			value = 10.0;
+		}
+		else if (choice.contentEquals("super")) {
+			value = 0.666667;
+		}
+		else {
+			value = 0;
+		}
+		return value;
 	}
 	
 
